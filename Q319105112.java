f913d35e8c1f80e337fcebd7339dbe5b3e7e3dc3
kareena1821 <83473756+kareena1821@users.noreply.github.com>
2022-10-18 21:38:21 +0530

    q3

---
+++ Q319105112.java
@@ -0,0 +1,16 @@
+//19105112
+//Write a program to sort strings (using library function).
+
+import java.util.*;
+public class Q319105112 {
+ 
+public static void main(String args[]){
+    String s = "kareenagupta"; //string input
+    char []charArray = s.toCharArray();//converted to character array to apply inbuilt func
+    Arrays.sort(charArray);//sorted the character array
+    System.out.println(String.valueOf(charArray));//print string output
+
+
+}
+
+}
